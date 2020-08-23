6b6b5aaee5efa772f2495c3b90ed125345fdf4c6
sarvan0506 <sarvan0506@gmail.com>
2020-08-23 07:30:00 +0530

    Update bring_env.bat

--- bring_env.bat
+++ bring_env.bat
@@ -1,3 +1,3 @@
 
 sudo docker rm cuda_torch -f
-sudo docker run --gpus all -p 8091:8091 -it -d --ipc=host --volume-driver=nfs -v /home/username:/src --restart always --name cuda_torch nvidia_anaconda_pytorch:v1
+sudo docker run --gpus all -p 8091:8091 -it -d --ipc=host --volume-driver=nfs -v \home\username:\src --restart always --name cuda_torch nvidia_anaconda_pytorch:v1
