12988c9702ec1e830e7047504bc3087f49251556
sarvan0506 <sarvan0506@gmail.com>
2020-08-23 07:26:21 +0530

    Update bring_env.sh

--- bring_env.sh
+++ bring_env.sh
@@ -1,2 +1,2 @@
-sudo docker rm nvana -f
-sudo docker run --gpus all -p 8091:8091 -it -d --ipc=host --volume-driver=nfs -v /home/saravana:/src --restart always --name nvana nvidia_anaconda:v1
+sudo docker rm cuda_torch -f
+sudo docker run --gpus all -p 8091:8091 -it -d --ipc=host --volume-driver=nfs -v /home/saravana:/src --restart always --name cuda_torch nvidia_anaconda_torch:v1
